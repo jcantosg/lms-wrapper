0d6015f3e1debe827114e34371a9d9b49ce2b515
pablo-rufat-secture <107040142+pablo-rufat-secture@users.noreply.github.com>
2024-01-29 12:53:49 +0100

    feat(businessunit): add isActive to business unit detail (#143)

--- src/sga/business-unit/infrastructure/controller/get-business-unit/get-business-unit.response.ts
+++ src/sga/business-unit/infrastructure/controller/get-business-unit/get-business-unit.response.ts
@@ -37,6 +37,7 @@ export class GetBusinessUnitResponse {
           return GetExaminationCenterResponse.create(examinationCenter);
         },
       ),
+      isActive: businessUnit.isActive,
     };
   }
 }
