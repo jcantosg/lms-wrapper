022b071450313ea6cbace0cc127f5fbbf8e2a418
Jose Cantos <97616580+jcantosg@users.noreply.github.com>
2024-02-01 12:33:55 +0100

    feat(businessunit): add fields to business unit detail response (#160)

--- src/sga/business-unit/infrastructure/controller/get-business-unit/get-virtual-campus.response.ts
+++ src/sga/business-unit/infrastructure/controller/get-business-unit/get-virtual-campus.response.ts
@@ -1,17 +1,21 @@
 import { VirtualCampus } from '#business-unit/domain/entity/virtual-campus.entity';
 
 export interface VirtualCampusResponse {
+  id: string;
   name: string;
   code: string;
   isMain: boolean;
+  isActive: boolean;
 }
 
 export class GetVirtualCampusResponse {
   static create(virtualCampus: VirtualCampus): VirtualCampusResponse {
     return {
+      id: virtualCampus.id,
       name: virtualCampus.name,
       code: virtualCampus.code,
       isMain: virtualCampus.isMain,
+      isActive: virtualCampus.isActive,
     };
   }
 }
