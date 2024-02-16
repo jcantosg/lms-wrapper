9c6c29211c8f47ec77497933e3628e29e8358d08
samuelSecture <samuel@secture.com>
2024-02-16 15:28:19 +0100

    fix: use body parser as limit

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from 'nestjs-pino';
 import { VERSION_NEUTRAL, VersioningType } from '@nestjs/common';
-import { json } from 'express';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,8 +15,9 @@ async function bootstrap() {
   });
   app.enableCors();
 
-  app.use('/', json({ limit: '50mb' }));
+  app.use(bodyParser.json({ limit: '50mb' }));
 
   await app.listen(3000);
 }
+
 bootstrap();
