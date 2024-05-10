60577126784f6c8f538af80bb1234f5943631f4c
pablo-rufat-secture <107040142+pablo-rufat-secture@users.noreply.github.com>
2024-05-10 11:02:40 +0200

    feat(subject): fix handler (#505)

--- src/sga/academic-offering/applicaton/subject/get-subjects-by-business-unit/get-subjects-by-business-unit.handler.ts
+++ src/sga/academic-offering/applicaton/subject/get-subjects-by-business-unit/get-subjects-by-business-unit.handler.ts
@@ -2,18 +2,17 @@ import { QueryHandler } from '#shared/domain/bus/query.handler';
 import { SubjectRepository } from '#academic-offering/domain/repository/subject.repository';
 import { GetSubjectsByBusinessUnitQuery } from '#academic-offering/applicaton/subject/get-subjects-by-business-unit/get-subjects-by-business-unit.query';
 import { Subject } from '#academic-offering/domain/entity/subject.entity';
-import { BusinessUnit } from '#business-unit/domain/entity/business-unit.entity';
 import { BusinessUnitNotFoundException } from '#shared/domain/exception/business-unit/business-unit/business-unit-not-found.exception';
 
 export class GetSubjectsByBusinessUnitHandler implements QueryHandler {
   constructor(private readonly repository: SubjectRepository) {}
 
   async handle(query: GetSubjectsByBusinessUnitQuery): Promise<Subject[]> {
-    let businessUnitBelongsToUser = false;
-    query.adminUser.businessUnits.forEach((businessUnit: BusinessUnit) => {
-      businessUnitBelongsToUser = businessUnit.id === query.businessUnitId;
-    });
-    if (!businessUnitBelongsToUser) {
+    if (
+      !query.adminUser.businessUnits
+        .map((bu) => bu.id)
+        .includes(query.businessUnitId)
+    ) {
       throw new BusinessUnitNotFoundException();
     }
 
