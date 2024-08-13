7ef94fae368013f97b2e8a822076821dcf4d8ba8
Samuel Sánchez Álvarez <103497378+samuelSecture@users.noreply.github.com>
2024-08-13 12:59:58 +0000

    feat(student-360): add get countries endpoint (#905)

--- src/shared/shared/controllers.ts
+++ src/shared/shared/controllers.ts
@@ -2,10 +2,12 @@ import { HealthController } from '#shared/infrastructure/controller/health.contr
 import { GetCountryController } from '#shared/infrastructure/controller/country/get-countries.controller';
 import { GetProvincesController } from '#shared/infrastructure/controller/province/get-provinces.controller';
 import { communicationControllers } from '#shared/infrastructure/controller/communication/controllers';
+import { GetCountryStudentController } from '#shared/infrastructure/controller/country/get-student-countries.controller';
 
 export const controllers = [
   HealthController,
   GetCountryController,
   GetProvincesController,
   ...communicationControllers,
+  GetCountryStudentController,
 ];
